5f905828c190212a39defc3707cd2e7fadf0f3f2
Will <41606834+developer-will@users.noreply.github.com>
2019-09-27 09:27:00 +1000

    Fixes #9. Movement was previously being adjusted based on player location and absolute floats etc. I noticed that sometimes movement was not doing that and assumed that it was because character was perfect distance, which I assumed to be movespeed, and testing confirmed. This could be improved by importing the player movespeed and size and calculating it dynamically for the creature, but for now this works. Given that the player is 0.5, movespeed is 0.3

--- Resources/Scripts/Tiles/wall.lua
+++ Resources/Scripts/Tiles/wall.lua
@@ -5,31 +5,19 @@ right = 0
 down = 1
 left = 2
 up = 3
-function wallTile:onEnter(x, y, lx, ly, dir) --lx/ly = player absolute floats, x/y = tile coords
-    if dir == right then
-        player.x = player.x - ((lx - x) + 0.01)
+function wallTile:onEnter(x, y, dir) --lx/ly = player absolute floats, x/y = tile coords
+    if dir == right then --left
+        player.x = x - 0.53
     end
-    if dir == left then
-        player.x = player.x + (1 - (lx - x) + 0.01)
+    if dir == left then --right
+        player.x = x + 1.03
     end
-    if dir == up then
-        player.y = player.y + (1 - (ly - y) + 0.01)
+    if dir == up then --bottom
+        player.y = y + 1.03
     end
-    if dir == down then
-        player.y = player.y - ((ly - y) + 0.01)
+    if dir == down then --top
+        player.y = y - 0.53
     end
-    --[[if lx > x and player.x < x then
-        player.x = player.x - ((lx - x) * 2)
-        return
-    elseif lx > x and player.x + player.w > x then
-        player.x = player.x + ((lx - x) - (player.w*2))
-        return
-    else if ly > y then
-        --player.y = y + 1 + 0.01
-        return
-    else if ly < y then
-        --player:setPos(player.x, y + 1.01)
-    end--]]
 end
 
 tiles:register(wallTile)
